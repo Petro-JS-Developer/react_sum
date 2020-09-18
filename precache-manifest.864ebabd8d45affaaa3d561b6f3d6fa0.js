13e6618b0c7f482e55f93a1b3027541997117e9a
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-09-18 16:19:47 +0300

    Updates

---
+++ precache-manifest.864ebabd8d45affaaa3d561b6f3d6fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0857b33a33b977f4e3f1824b336557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f62ff670276ec47fd2cd",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "c40b73c1e8c4b77a5cee",
+    "url": "./static/js/2.d383e718.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d383e718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62ff670276ec47fd2cd",
+    "url": "./static/js/main.3571c5e8.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
